903d6a5576a5b5415d080b584acc9a83434029f9
Alex Meyer <alexwreyem@gmail.com>
2019-02-28 19:39:12 -0500

    add global tty-color script

---
+++ .config/_colorschemes/colors-tty.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# base16-gruvbox-hard for TTYs
+
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P01d2021
+                 \e]P1fb4934
+                 \e]P2b8bb26
+                 \e]P3fabd2f
+                 \e]P483a598
+                 \e]P5d3869b
+                 \e]P68ec07c
+                 \e]P7d5c4a1
+                 \e]P8665c54
+                 \e]P9fb4934
+                 \e]PAb8bb26
+                 \e]PBfabd2f
+                 \e]PC83a598
+                 \e]PDd3869b
+                 \e]PE8ec07c
+                 \e]PFfbf1c7
+                 \ec'
